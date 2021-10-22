e7ad6536787b0011dc9a8005355f9b62d8dfb448
eduvlemes <edu.vlemes@gmail.com>
2021-10-21 21:36:16 -0300

    get/points

--- functions/routes/get/points.js
+++ functions/routes/get/points.js
@@ -1,20 +1,25 @@
 const axios = require('axios')
 const getAppData = require('./../../lib/store-api/get-app-data')
 
-getAppData({appSdk, storeId}).then(appData => {
-  res.send(appData);
-  // const { params, application } = req.body
-  // const { storeId } = req
-  // const response = {}
-  // const appData = Object.assign({}, application.data, application.hidden_data)
-  // if (appData.instancia) {
-  //   return axios.post(appData.instancia, {
-  //     id: appData.id,
-  //     token: appData.token,
-  //     cpf: req.body.cpf
-  //   })
-  //     .then(({ data }) => res.send(data))
-  //     .catch(console.error)
-  // }
-  // res.send({})
-})
+// getAppData({appSdk, storeId}).then(appData => {
+// const { params, application } = req.body
+//   const { storeId } = req
+//   const response = {}
+//   const appData = Object.assign({}, application.data, application.hidden_data)
+//   if (appData.instancia) {
+//     return axios.post(appData.instancia, {
+//       id: appData.id,
+//       token: appData.token,
+//       cpf: req.body.cpf
+//     })
+//       .then(({ data }) => res.send(data))
+//       .catch(console.error)
+//   }
+//   res.send({})
+// })
+exports.post = ({ appSdk, admin }, req, res) => {  
+  getAppData({appSdk, storeId}).then(appData => {
+    res.send(appData);
+
+  })
+}
