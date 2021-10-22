b91f7096b31b53c0ff4265d493c9fc2cd15b1999
eduvlemes <edu.vlemes@gmail.com>
2021-10-21 21:32:06 -0300

    get/points

---
+++ functions/routes/get/points.js
@@ -0,0 +1,20 @@
+const axios = require('axios')
+const getAppData = require('./../../lib/store-api/get-app-data')
+
+getAppData({appSdk, storeId}).then(appData => {
+  res.send(appData);
+  // const { params, application } = req.body
+  // const { storeId } = req
+  // const response = {}
+  // const appData = Object.assign({}, application.data, application.hidden_data)
+  // if (appData.instancia) {
+  //   return axios.post(appData.instancia, {
+  //     id: appData.id,
+  //     token: appData.token,
+  //     cpf: req.body.cpf
+  //   })
+  //     .then(({ data }) => res.send(data))
+  //     .catch(console.error)
+  // }
+  // res.send({})
+})
