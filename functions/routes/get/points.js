31ceac5ab55eac6cb9b1c90853e4ca08d5ba3a1d
eduvlemes <edu.vlemes@gmail.com>
2021-10-29 14:24:52 -0300

    getpoints_30

--- functions/routes/get/points.js
+++ functions/routes/get/points.js
@@ -1,3 +1,33 @@
+// const axios = require('axios')
+// const getAppData = require('./../../lib/store-api/get-app-data')
+
+
+// exports.post = ({ appSdk }, req, res) => {  
+//   const { storeId } = req.body
+//   getAppData({appSdk, storeId}).then(appData => {
+//     if (appData.instancia) {
+//       const params = {
+//         id : appData.id,
+//         token : appData.token,
+//         cmd : "get_points",
+//         cpf : "43335443608"
+//       }      
+//       return axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', {
+//         params,
+//         headers: {
+//           'Content-Type': 'multipart/form-data'
+//         }
+//       })
+//       .then(({ data }) => res.send(data))
+//       .catch(console.error)
+//     }  
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     res.send(err)
+//   })
+// }
+
 const axios = require('axios')
 const getAppData = require('./../../lib/store-api/get-app-data')
 
@@ -5,25 +35,29 @@ const getAppData = require('./../../lib/store-api/get-app-data')
 exports.post = ({ appSdk }, req, res) => {  
   const { storeId } = req.body
   getAppData({appSdk, storeId}).then(appData => {
-    if (appData.instancia) {
-      const params = {
+    if(appData.instancia){
+      let data = {
         id : appData.id,
         token : appData.token,
         cmd : "get_points",
+        // cpf : req.body.cpf
+        // id : id,
+        // token : token,
+        // cmd : cmd,
         cpf : "43335443608"
-      }      
-      return axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', {
-        params,
+      }
+      axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', data , {
         headers: {
-          'Content-Type': 'multipart/form-data'
+          'Content-Type': 'application/json'
         }
       })
-      .then(({ data }) => res.send(data))
-      .catch(console.error)
-    }  
+      .then(({data}) => {res.send(data)})
+      .catch((err) => {
+        {res.send(err)}
+      })
+    }
   })
   .catch(err => {
-    console.log(err)
     res.send(err)
   })
 }
