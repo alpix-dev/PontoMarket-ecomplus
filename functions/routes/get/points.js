1322c9db94f2377889de80811fe6af77c934bff8
eduvlemes <edu.vlemes@gmail.com>
2021-10-29 14:09:38 -0300

    getpoints_29

--- functions/routes/get/points.js
+++ functions/routes/get/points.js
@@ -5,48 +5,22 @@ const getAppData = require('./../../lib/store-api/get-app-data')
 exports.post = ({ appSdk }, req, res) => {  
   const { storeId } = req.body
   getAppData({appSdk, storeId}).then(appData => {
-    if(appData.instancia){
-      // let data = {
-      //   id : appData.id,
-      //   token : appData.token,
-      //   cmd : "get_points",
-      //   // cpf : req.body.cpf
-      //   // id : id,
-      //   // token : token,
-      //   // cmd : cmd,
-      //   cpf : "43335443608"
-      // }
-      let formData = new FormData();
-      formData.append('id', appData.id)
-      formData.append('token', appData.token)
-      formData.append('cmd', "get_points")
-      formData.append('cpf', "43335443608")
-      
-      // return axios({
-      //   method:"post",
-      //   url: appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1',
-      //   data: formData,
-      //   headers: {'Content-Type': 'multipart/form-data'}
-      // }).then((response) => {
-      //   console.log(response)
-      //   res.send(response)
-      // })
-      // .catch((err) => {
-      //   console.log(err)
-      //   res.send(err)        
-      // })
-      try {
-        let clubeShow = axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', formData , {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          }
-        })
-        res.send(clubeShow)
-      }catch(e){
-        console.log(e)
-        res.send(e)
-      }     
-    }
+    if (appData.instancia) {
+      const params = {
+        id : appData.id,
+        token : appData.token,
+        cmd : "get_points",
+        cpf : "43335443608"
+      }      
+      return axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', {
+        params,
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+      .then(({ data }) => res.send(data))
+      .catch(console.error)
+    }  
   })
   .catch(err => {
     console.log(err)
