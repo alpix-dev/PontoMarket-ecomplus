3d5bd4d7eeb58e839eff5929501acd09911e358c
Leonardo Matos <leomp120894@gmail.com>
2021-10-29 14:39:20 -0300

    Update points.js

--- functions/routes/get/points.js
+++ functions/routes/get/points.js
@@ -31,33 +31,38 @@
 const axios = require('axios')
 const getAppData = require('./../../lib/store-api/get-app-data')
 
-
 exports.post = ({ appSdk }, req, res) => {  
   const { storeId } = req.body
-  getAppData({appSdk, storeId}).then(appData => {
-    if(appData.instancia){
+  getAppData({ appSdk, storeId }).then(appData => {
+    if (appData.instancia) {
       let data = {
-        id : appData.id,
-        token : appData.token,
-        cmd : "get_points",
+        id: appData.id,
+        token: appData.token,
+        cmd: "get_points",
         // cpf : req.body.cpf
         // id : id,
         // token : token,
         // cmd : cmd,
-        cpf : "43335443608"
+        cpf: "43335443608"
       }
-      axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', data , {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(({data}) => {res.send(data)})
-      .catch((err) => {
-        {res.send(err)}
-      })
+      const url = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1`
+      console.log(JSON.stringify({ storeId, url, data }))
+      axios.post(url, data)
+        .then(({ data }) => {
+          res.send(data)
+        })
+        .catch(err => {
+          console.log(JSON.stringify({
+            url,
+            resStatus: err.response?.status,
+            resData: err.response?.data
+          }))
+          res.send(err.message)
+        })
     }
   })
   .catch(err => {
-    res.send(err)
+    console.error(err)
+    res.send(err.message)
   })
 }
