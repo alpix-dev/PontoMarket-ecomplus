8f4e34ebbbe4beb83396b6e14ce8001523b7c15f
Leonardo Matos <leomp120894@gmail.com>
2021-10-29 14:52:49 -0300

    Update points.js

--- functions/routes/get/points.js
+++ functions/routes/get/points.js
@@ -29,25 +29,25 @@
 // }
 
 const axios = require('axios')
+const FormData = require('form-data')
 const getAppData = require('./../../lib/store-api/get-app-data')
 
 exports.post = ({ appSdk }, req, res) => {  
   const { storeId } = req.body
   getAppData({ appSdk, storeId }).then(appData => {
     if (appData.instancia) {
-      let data = {
-        id: appData.id,
-        token: appData.token,
-        cmd: "get_points",
-        // cpf : req.body.cpf
-        // id : id,
-        // token : token,
-        // cmd : cmd,
-        cpf: "43335443608"
-      }
+      const data = new FormData()
+      data.append('id', appData.id)
+      data.append('token', appData.token)
+      data.append('cmd', "get_points")
+      data.append('cpf', "43335443608")
       const url = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1`
       console.log(JSON.stringify({ storeId, url, data }))
-      axios.post(url, data)
+      axios.post(url, data, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
         .then(({ data }) => {
           res.send(data)
         })
