19c4e5fc4e1044f6f8fd401962afc1c66c218d6c
eduvlemes <edu.vlemes@gmail.com>
2021-10-29 12:08:23 -0300

    getpoints_19

--- functions/routes/get/points.js
+++ functions/routes/get/points.js
@@ -6,26 +6,41 @@ exports.post = ({ appSdk }, req, res) => {
   const { storeId } = req.body
   getAppData({appSdk, storeId}).then(appData => {
     if(appData.instancia){
-      let data = {
-        id : appData.id,
-        token : appData.token,
-        cmd : "get_points",
-        // cpf : req.body.cpf
-        // id : id,
-        // token : token,
-        // cmd : cmd,
-        cpf : "43335443608"
-      }
-      console.log(data)
-      axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', data , {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-      .then(({data}) => {res.send(data)})
+      // let data = {
+      //   id : appData.id,
+      //   token : appData.token,
+      //   cmd : "get_points",
+      //   // cpf : req.body.cpf
+      //   // id : id,
+      //   // token : token,
+      //   // cmd : cmd,
+      //   cpf : "43335443608"
+      // }
+      let formData = new FormData();
+      formData.append('id', appData.id)
+      formData.append('token', appData.token)
+      formData.append('cmd', "get_points")
+      formData.append('cpf', "43335443608")
+      
+      axios({
+        method:"post",
+        url: appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1',
+        data: formData,
+        headers: {'Content-Type': 'multipart/form-data'}
+      }).then(({data}) => {res.send(data)})
       .catch((err) => {
         {res.send(err)}
       })
+
+      // axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', data , {
+      //   headers: {
+      //     'Content-Type': 'multipart/form-data'
+      //   }
+      // })
+      // .then(({data}) => {res.send(data)})
+      // .catch((err) => {
+      //   {res.send(err)}
+      // })
     }
   })
   .catch(err => {
