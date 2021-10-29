e376db8b945211474786fa6abe536ae2ea558180
eduvlemes <edu.vlemes@gmail.com>
2021-10-29 01:38:11 -0300

    getpoints_4

--- functions/routes/get/points.js
+++ functions/routes/get/points.js
@@ -21,19 +21,22 @@ exports.post = ({ appSdk }, req, res) => {
   const { storeId } = req.body
   getAppData({appSdk, storeId})
   .then(appData => {
+    console.log('entrou 0')
     //res.send(appData);
-    console.log(appData)
+    //console.log(appData)
     axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', {
         id : appData.id,
         token : appData.token,
         cmd : "get_points",
         cpf : req.body.cpf
         // id_location: mail.replyTo
-    }).then(pmarketResponse => {
-      console.log(pmarketResponse)
-      res.send(pmarketResponse)
+    }).then(data => {
+      console.log('entrou 1')
+      //console.log(data)
+      res.send(data)
     }).catch(err =>{
-      console.log(err)
+      console.log('entrou 2')
+      //console.log(err)
       res.send(err)  
     })
 
