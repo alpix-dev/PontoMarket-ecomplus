34466a387a7808f3c544f250418f73686492a94a
eduvlemes <edu.vlemes@gmail.com>
2021-10-29 01:47:19 -0300

    getpoints_6

--- functions/routes/get/points.js
+++ functions/routes/get/points.js
@@ -19,27 +19,18 @@ const getAppData = require('./../../lib/store-api/get-app-data')
 // })
 exports.post = ({ appSdk }, req, res) => {  
   const { storeId } = req.body
-  getAppData({appSdk, storeId})
-  .then(appData => {
-    console.log('entrou 0')
-    res.send(appData);
-    //console.log(appData)
-    axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', {
+  getAppData({appSdk, storeId}).then(appData => {
+    const response = {}
+    const appData = Object.assign({}, application.data, application.hidden_data)
+    if(appData.instancia){
+      return axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', {
         id : appData.id,
         token : appData.token,
         cmd : "get_points",
         cpf : req.body.cpf
-        // id_location: mail.replyTo
-    }).then(data => {
-      console.log('entrou 1')
-      //console.log(data)
-      res.send(data)
-    }).catch(err =>{
-      console.log('entrou 2')
-      //console.log(err)
-      res.send(err)  
-    })
-
+      }).then(({data}) => {res.send(data)})
+      .catch(console.error)
+    }
   })
   .catch(err => {
     res.send(err)
