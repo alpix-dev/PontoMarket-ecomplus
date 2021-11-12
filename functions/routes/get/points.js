42f8a3b442cb4c8b4afcc64df64da32b097c26e2
eduvlemes <edu.vlemes@gmail.com>
2021-11-11 23:35:24 -0300

    remove content-type

--- functions/routes/get/points.js
+++ functions/routes/get/points.js
@@ -1,33 +1,3 @@
-// const axios = require('axios')
-// const getAppData = require('./../../lib/store-api/get-app-data')
-
-
-// exports.post = ({ appSdk }, req, res) => {  
-//   const { storeId } = req.body
-//   getAppData({appSdk, storeId}).then(appData => {
-//     if (appData.instancia) {
-//       const params = {
-//         id : appData.id,
-//         token : appData.token,
-//         cmd : "get_points",
-//         cpf : "43335443608"
-//       }      
-//       return axios.post(appData.instancia + '/cgi-bin/webworks/bin/sharkview_api_v1', {
-//         params,
-//         headers: {
-//           'Content-Type': 'multipart/form-data'
-//         }
-//       })
-//       .then(({ data }) => res.send(data))
-//       .catch(console.error)
-//     }  
-//   })
-//   .catch(err => {
-//     console.log(err)
-//     res.send(err)
-//   })
-// }
-
 const axios = require('axios')
 const FormData = require('form-data')
 const getAppData = require('./../../lib/store-api/get-app-data')
@@ -44,9 +14,9 @@ exports.post = ({ appSdk }, req, res) => {
       const url = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1`
       console.log(JSON.stringify({ storeId, url, data }))
       axios.post(url, data, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
+        //headers: {
+//          'Content-Type': 'multipart/form-data'
+        //}
       })
         .then(({ data }) => {
           res.send(data)
