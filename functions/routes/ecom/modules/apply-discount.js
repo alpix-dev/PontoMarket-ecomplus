1ce221d2486b140d1e81298d9d3363ba78f8d9b0
Leonardo Matos <leomp120894@gmail.com>
2021-11-16 13:57:31 -0300

    Update apply-discount.js

--- functions/routes/ecom/modules/apply-discount.js
+++ functions/routes/ecom/modules/apply-discount.js
@@ -20,40 +20,46 @@ exports.post = ({ appSdk, admin }, req, res) => {
   if (appData.available_extra_discount) {
     response.available_extra_discount = appData.available_extra_discount
   }
-  if (params.discount_coupon) {
-    // should match discount by coupon code
-  }
 
-  getAppData({appSdk, storeId}).then(appData => {
-    if(appData.instancia){
-      const url = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=43335443608&id_location=`
-      axios.get(url)
-      .then(({ data }) => {
-        //res.send(data)
-        let offer = data.prize_list.filter(el => el.id_prize == params.discount_coupon)
-        if(offer[0].prize_value > 0){
-          response.discount_rule = {
-            label: offer[0].name,
-            extra_discount: {
-              value: offer[0].prize_value,
-              flags: ['clube-show']
-            }
-          }
+  if (params.customer?._id) {
+    // Should match selected discount by customer ID
+    return admin.firestore().doc(`prizes/${storeId}_${params.customer._id}`).get(documentSnapshot => {
+      if (documentSnapshot.exist) {
+        const prizeId = documentSnapshot.get('selected_prize_id')
+        if (prizeId) {
+          // Double check discount available on CRM
+          const crmUrl = documentSnapshot.get('crm_url')
+          return axios.get(crmUrl)
+            .then(({ data }) => {
+              const prize = data.prize_list?.find(prize => (prize.id_prize == prizeId))
+              if (prize && prize.prize_value > 0) {
+                response.discount_rule = {
+                  label: prize.name,
+                  description: `Resgate de pontos ID ${prize.id_prize}`,
+                  extra_discount: {
+                    value: prize.prize_value,
+                    flags: ['clube-show', `${String(prize.id_prize).slice(0, 20)}`]
+                  }
+                }
+              }
+              res.send(response)
+            })
+            .catch(err => {
+              console.log(JSON.stringify({
+                url,
+                resStatus: err.response?.status,
+                resData: err.response?.data
+              }))
+              res.status(409).send({
+                error: '1',
+                message: err.message
+              })
+            })
         }
-      })
-      .catch(err => {
-        console.log(JSON.stringify({
-          url,
-          resStatus: err.response?.status,
-          resData: err.response?.data
-        }))
-        res.send(err.message)
-      })
-    }
-  })
-  .catch(err => {
-    res.send(err)
-  })
+      }
+      res.send(response)
+    })
+  }
 
   /* DO THE STUFF HERE TO FILL RESPONSE OBJECT WITH DISCOUNT OPTIONS */
 
