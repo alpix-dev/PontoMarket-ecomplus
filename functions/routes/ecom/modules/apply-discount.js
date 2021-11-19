b18ec2b3fcfef056684539ec2c9b5df6a133897e
eduvlemes <edu.vlemes@gmail.com>
2021-11-19 16:45:28 -0300

    apply-discount 13

--- functions/routes/ecom/modules/apply-discount.js
+++ functions/routes/ecom/modules/apply-discount.js
@@ -23,21 +23,25 @@ exports.post = ({ appSdk, admin }, req, res) => {
   console.log(params.customer)
   console.log(params.customer?._id)
   if (params.customer?._id) {
-    
+      console.log('--- checkpoint a')
     // Should match selected discount by customer ID
     return admin.firestore().doc(`prizes/${storeId}_${params.customer._id}`).get(documentSnapshot => {
-      console.log(documentSnapshot)
+      console.log('--- checkpoint b')
       if (documentSnapshot.exist) {
+        console.log('--- checkpoint b 1')
         const prizeId = documentSnapshot.get('selected_prize_id')
         if (prizeId) {
+          console.log('--- checkpoint b 2')
           // Double check discount available on CRM
           const docNumber = documentSnapshot.get('doc_number')
           //const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=${docNumber}&id_location=`
           const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=43335443608&id_location=`
           return axios.get(crmUrl)
             .then(({ data }) => {
+              console.log('--- checkpoint c')
               const prize = data.prize_list?.find(prize => (prize.id_prize == prizeId))
               if (prize && prize.prize_value > 0) {
+                console.log('--- checkpoint d')
                 response.discount_rule = {
                   label: prize.name,
                   description: `Resgate de pontos ID ${prize.id_prize}`,
