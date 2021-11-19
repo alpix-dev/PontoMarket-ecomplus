3d8fe69539c404881afacab4f49523b7439adef1
eduvlemes <edu.vlemes@gmail.com>
2021-11-19 15:15:04 -0300

    apply-discount 10

--- functions/routes/ecom/modules/apply-discount.js
+++ functions/routes/ecom/modules/apply-discount.js
@@ -17,65 +17,22 @@ exports.post = ({ appSdk, admin }, req, res) => {
   // merge all app options configured by merchant
   const appData = Object.assign({}, application.data, application.hidden_data)
 
-  //console.log(params)
-  console.log(params.customer)
+  if (appData.available_extra_discount) {
+    response.available_extra_discount = appData.available_extra_discount
+  }
+
   if (params.customer?._id) {
     // Should match selected discount by customer ID
-    // admin.firestore().doc(`prizes/${storeId}_${params.customer._id}`).get(documentSnapshot => {
-    //   if (documentSnapshot.exist) {
-    //     console.log(documentSnapshot)
-    //     const prizeId = documentSnapshot.get('selected_prize_id')
-    //     if (prizeId) {
-    //       // Double check discount available on CRM
-    //       const docNumber = documentSnapshot.get('doc_number')
-    //       //const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=${docNumber}&id_location=${appData.location_id}`
-    //       const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=43335443608&id_location=${appData.location_id}`
-    //       console.log(crmUrl)
-    //       axios.get(crmUrl)
-    //         .then(({ data }) => {
-    //           console.log(data)
-    //           const prize = data.prize_list?.find(prize => (prize.id_prize == prizeId))
-    //           if (prize && prize.prize_value > 0) {
-    //             response.discount_rule = {
-    //               label: prize.name,
-    //               description: `Resgate de pontos ID ${prize.id_prize}`,
-    //               extra_discount: {
-    //                 value: prize.prize_value,
-    //                 flags: ['clube-show', `${String(prize.id_prize).slice(0, 20)}`]
-    //               }
-    //             }
-    //           }
-    //           res.send(response)
-    //         })
-    //         .catch(err => {
-    //           console.log(JSON.stringify({
-    //             url,
-    //             resStatus: err.response?.status,
-    //             resData: err.response?.data
-    //           }))
-    //           res.status(409).send({
-    //             error: '1',
-    //             message: err.message
-    //           })
-    //         })
-    //     }
-    //   }
-    //   res.send(response)
-    // })
-    
-    admin.firestore().doc(`prizes/${storeId}_${params.customer._id}`).get()
-    .then(function(documentSnapshot){
-      console.log(documentSnapshot)
+    return admin.firestore().doc(`prizes/${storeId}_${params.customer._id}`).get(documentSnapshot => {
+      if (documentSnapshot.exist) {
         const prizeId = documentSnapshot.get('selected_prize_id')
         if (prizeId) {
           // Double check discount available on CRM
           const docNumber = documentSnapshot.get('doc_number')
-          //const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=${docNumber}&id_location=${appData.location_id}`
-          const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=43335443608&id_location=${appData.location_id}`
-          console.log(crmUrl)
-          axios.get(crmUrl)
+          //const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=${docNumber}&id_location=`
+          const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=43335443608&id_location=`
+          return axios.get(crmUrl)
             .then(({ data }) => {
-              console.log(data)
               const prize = data.prize_list?.find(prize => (prize.id_prize == prizeId))
               if (prize && prize.prize_value > 0) {
                 response.discount_rule = {
@@ -100,7 +57,9 @@ exports.post = ({ appSdk, admin }, req, res) => {
                 message: err.message
               })
             })
-        }      
+        }
+      }
+      res.send(response)
     })
   }
 
@@ -108,7 +67,6 @@ exports.post = ({ appSdk, admin }, req, res) => {
 
   /**
    * Sample snippets:
-
   // set discount value
   response.discount_rule = {
     label: 'X Campaign',
@@ -117,7 +75,6 @@ exports.post = ({ appSdk, admin }, req, res) => {
       flags: ['x-coupon']
     }
   }
-
   */
 
   res.send(response)
