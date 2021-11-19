823d0e9a7c53504038132dac6c11f3e582ba08df
eduvlemes <edu.vlemes@gmail.com>
2021-11-19 17:13:27 -0300

    apply-discount 15

--- functions/routes/ecom/modules/apply-discount.js
+++ functions/routes/ecom/modules/apply-discount.js
@@ -24,50 +24,88 @@ exports.post = ({ appSdk, admin }, req, res) => {
   console.log(params.customer?._id)
   if (params.customer?._id) {
       console.log('--- checkpoint a')
-    // Should match selected discount by customer ID
-    admin.firestore().doc(`prizes/${storeId}_${params.customer._id}`).get(documentSnapshot => {
-      console.log('--- checkpoint b')
-      if (documentSnapshot.exist) {
-        console.log('--- checkpoint b 1')
-        const prizeId = documentSnapshot.get('selected_prize_id')
-        if (prizeId) {
-          console.log('--- checkpoint b 2')
-          // Double check discount available on CRM
-          const docNumber = documentSnapshot.get('doc_number')
-          //const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=${docNumber}&id_location=`
-          const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=43335443608&id_location=`
-          axios.get(crmUrl)
-            .then(({ data }) => {
-              console.log('--- checkpoint c')
-              const prize = data.prize_list?.find(prize => (prize.id_prize == prizeId))
-              if (prize && prize.prize_value > 0) {
-                console.log('--- checkpoint d')
-                response.discount_rule = {
-                  label: prize.name,
-                  description: `Resgate de pontos ID ${prize.id_prize}`,
-                  extra_discount: {
-                    value: prize.prize_value,
-                    flags: ['clube-show', `${String(prize.id_prize).slice(0, 20)}`]
-                  }
+    admin.firestore().doc(`prizes/${storeId}_${params.customer._id}`).get()
+    .then(function(result){
+      const reg = result.data()
+      if (reg.selected_prize_id) {
+        console.log('--- checkpoint b 2')
+        // Double check discount available on CRM
+        const docNumber = reg.doc_number
+        const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=${docNumber}&id_location=`
+        //const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=43335443608&id_location=`
+        axios.get(crmUrl)
+          .then(({ data }) => {
+            console.log('--- checkpoint c')
+            const prize = data.prize_list?.find(prize => (prize.id_prize == prizeId))
+            if (prize && prize.prize_value > 0) {
+              console.log('--- checkpoint d')
+              response.discount_rule = {
+                label: prize.name,
+                description: `Resgate de pontos ID ${prize.id_prize}`,
+                extra_discount: {
+                  value: prize.prize_value,
+                  flags: ['clube-show', `${String(prize.id_prize).slice(0, 20)}`]
                 }
               }
-              res.send(response)
+            }
+            res.send(response)
+          })
+          .catch(err => {
+            console.log(JSON.stringify({
+              url,
+              resStatus: err.response?.status,
+              resData: err.response?.data
+            }))
+            res.status(409).send({
+              error: '1',
+              message: err.message
             })
-            .catch(err => {
-              console.log(JSON.stringify({
-                url,
-                resStatus: err.response?.status,
-                resData: err.response?.data
-              }))
-              res.status(409).send({
-                error: '1',
-                message: err.message
-              })
-            })
-        }
-      }
-      res.send(response)
+          })
+      }      
     })
+    // admin.firestore().doc(`prizes/${storeId}_${params.customer._id}`).get(documentSnapshot => {
+    //   console.log('--- checkpoint b')
+    //   if (documentSnapshot.exist) {
+    //     console.log('--- checkpoint b 1')
+    //     const prizeId = documentSnapshot.get('selected_prize_id')
+    //     if (prizeId) {
+    //       console.log('--- checkpoint b 2')
+    //       // Double check discount available on CRM
+    //       const docNumber = documentSnapshot.get('doc_number')
+    //       //const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=${docNumber}&id_location=`
+    //       const crmUrl = `${appData.instancia}/cgi-bin/webworks/bin/sharkview_api_v1?id=${appData.id}&token=${appData.token}&cmd=get_points&cpf=43335443608&id_location=`
+    //       axios.get(crmUrl)
+    //         .then(({ data }) => {
+    //           console.log('--- checkpoint c')
+    //           const prize = data.prize_list?.find(prize => (prize.id_prize == prizeId))
+    //           if (prize && prize.prize_value > 0) {
+    //             console.log('--- checkpoint d')
+    //             response.discount_rule = {
+    //               label: prize.name,
+    //               description: `Resgate de pontos ID ${prize.id_prize}`,
+    //               extra_discount: {
+    //                 value: prize.prize_value,
+    //                 flags: ['clube-show', `${String(prize.id_prize).slice(0, 20)}`]
+    //               }
+    //             }
+    //           }
+    //           res.send(response)
+    //         })
+    //         .catch(err => {
+    //           console.log(JSON.stringify({
+    //             url,
+    //             resStatus: err.response?.status,
+    //             resData: err.response?.data
+    //           }))
+    //           res.status(409).send({
+    //             error: '1',
+    //             message: err.message
+    //           })
+    //         })
+    //     }
+    //   }
+    //   res.send(response)
+    // })
   }
 
   /* DO THE STUFF HERE TO FILL RESPONSE OBJECT WITH DISCOUNT OPTIONS */
